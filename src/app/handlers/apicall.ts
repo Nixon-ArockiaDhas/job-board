98492af4909599fc3beb14fd5d3059033e5c2fa2
Nixon <75479937+Nixon-ArockiaDhas@users.noreply.github.com>
2025-05-18 22:07:22 +0530

    Update apicall.ts

--- src/app/handlers/apicall.ts
+++ src/app/handlers/apicall.ts
@@ -1,6 +1,6 @@
 export const fetchJobsFromAPI = async () => {
   try {
-    const res = await fetch('http://localhost:3000/api/jobs');
+    const res = await fetch('https://job-board-eosin.vercel.app/api/jobs');
     if (!res.ok) throw new Error('Failed to fetch jobs');
     const data = await res.json();
     return data;
